35c3675a1f1f763dfd7cda0deabf8784be219750
Tharkus <lucasguilherme_@hotmail.com>
2026-02-23 16:47:40 -0300

    novas atividades sobre classes python

---
+++ Praticando Python/Python - aplicando a Orientacao a objetos/1_Mao_na_massa_classe_musica.py
@@ -0,0 +1,23 @@
+class Musica:
+    nome = ""
+    artista = ""
+    duracao = ""
+
+musica1 = Musica()
+musica1.nome = "Bohemian Rhapsody"
+musica1.artista = "Queen"
+musica1.duracao = "5:55"
+
+musica2 = Musica()
+musica2.nome = "Imagine"
+musica2.artista = "John Lennon"
+musica2.duracao = "3:03"
+
+musica3 = Musica()
+musica3.nome = "Hotel California"
+musica3.artista = "Eagles"
+musica3.duracao = "6:30"
+
+print(musica1.nome, "-", musica1.artista, "-", musica1.duracao)
+print(f"{musica2.nome} - {musica2.artista} - {musica2.duracao}")
+print(musica3.nome, musica3.artista, musica3.duracao)
---
+++ Praticando Python/Python - aplicando a Orientacao a objetos/1_Mao_na_massa_classe_musica_avancado.py
@@ -0,0 +1,15 @@
+class Musica:
+    def __init__(self, nome, artista, duracao):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+
+musica1 = Musica(nome='Bohemian Rhapsody', artista='Queen', duracao='5:55')
+musica2 = Musica(nome='Imagine', artista='John Lennon', duracao='3:03')
+musica3 = Musica(nome='Hotel California', artista='Eagles', duracao='6:30')
+
+musicas = [musica1, musica2, musica3]
+
+for musica in musicas:
+    print(f'{musica.nome} - {musica.artista} - {musica.duracao}')
+
