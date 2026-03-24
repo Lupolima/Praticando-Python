b98098867e83e5d146b4f3450d98c5b63aabb991
Lupolima <lucasguilherme_@hotmail.com>
2026-03-24 15:55:45 -0300

    Removendo arquivos desnecessários e adicionando nova implementação da classe Musica

--- Python - aplicando a Orientacao a objetos/2_Mao_na_massa_classe_musica_avancado.py
+++ Python - aplicando a Orientacao a objetos/2_Mao_na_massa_classe_musica_avancado.py
@@ -4,9 +4,9 @@ class Musica:
         self.artista = artista
         self.duracao = duracao
 
-musica1 = Musica(nome='Bohemian Rhapsody', artista='Queen', duracao='5:55')
-musica2 = Musica(nome='Imagine', artista='John Lennon', duracao='3:03')
-musica3 = Musica(nome='Hotel California', artista='Eagles', duracao='6:30')
+musica1 = Musica('Bohemian Rhapsody', 'Queen', '5:55')
+musica2 = Musica('Imagine', 'John Lennon', '3:03')
+musica3 = Musica('Hotel California', 'Eagles', '6:30')
 
 musicas = [musica1, musica2, musica3]
 
